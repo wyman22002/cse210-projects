5928ef21fc14ea474abfe03e02d851727f723c36
TannerWyman <tannerw2002@hotmail.com>
2023-02-27 15:10:39 -0700

    Compleated prepare assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 2, 8));
+        shapes.Add(new Circle("Green", 2));
+
+        foreach(Shape shape in shapes) {
+            double area = shape.GetArea();
+            string color = shape.GetColor();
+            Console.WriteLine($"Color:{color} Area:{area}.");
+        }
     }
 }
